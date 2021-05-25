c60453a97d23e45f02725f33b5e7a9004526a381
김동수 <dskim9752@gmail.com>
2021-05-25 17:28:50 +0900

    fix commit and history

---
+++ arduino/hm10_ble_v3/hm10_ble_v3.ino
@@ -0,0 +1,63 @@
+#include <SoftwareSerial.h>
+int Tx = 5; //전송 보내는핀
+int Rx = 4; //수신 받는핀
+int baud = 9600; //보드레이트
+unsigned long timestamp;
+SoftwareSerial BTSerial(Tx,Rx); //RX,TX
+
+
+void initAT(){    //초기 설정
+  BTSerial.println("AT+RENEW");    // renew module
+  delay(1000);
+  BTSerial.println("AT+NOTI0");    // set notification off
+  delay(1000); 
+  BTSerial.println("AT+ROLE1");    // set Master(Central) mode
+  delay(1000);
+  //BTSerial.println("AT+FILT0");  // set find all BLE modules
+  //delay(500);
+  BTSerial.println("AT+SHOW1");    // show name
+  delay(1500);
+  BTSerial.println("AT+IMME1");    // set IMME mode
+  delay(1500);
+  BTSerial.println("AT+RESET");    // reset module  
+  delay(1500);
+  BTSerial.println("AT");          // ready
+  delay(1500);
+}
+
+void atCommand(String at, unsigned int time){
+  if (millis() - timestamp > time*1000) {    //run every n seconds
+    BTSerial.println(at); 
+    timestamp = millis();
+   // Serial.println(tmp);
+  }
+}
+
+void setup(){
+  pinMode(7,OUTPUT);    // hm10 frezzing 임시조치 -  전원공급용
+  pinMode(6,OUTPUT);    // gnd
+  digitalWrite(7,HIGH);
+  digitalWrite(6,LOW);
+  Serial.begin(baud);
+  BTSerial.begin(baud);
+  delay(1000);
+  initAT(); 
+}
+
+void loop(){
+  atCommand("AT+DISC?",5);
+  if (BTSerial.available())   // Read from HM-10 and send to Serial Monitor
+    Serial.write(BTSerial.read());
+  if (Serial.available())    // Read from Serial Monitor and send to HM-10
+    BTSerial.write(Serial.read());
+}
+
+
+
+/*
+ if (millis() - timestamp > 5000) { //run every 5 seconds
+    BTSerial.println("AT-DISC?"); 
+     //Serial.println();
+    timestamp = millis();
+  }
+ */
---
+++ pi/scatter_pi.py
@@ -0,0 +1,74 @@
+import sys
+import glob
+import serial
+import requests
+
+
+BAUD = 9600
+URL = ""    #need URL
+item = "starbucks"
+count = -1
+
+
+def serial_ports():
+    """ Lists serial port names
+
+        :raises EnvironmentError:
+            On unsupported or unknown platforms
+        :returns:
+            A list of the serial ports available on the system
+
+    https://stackoverflow.com/questions/12090503/listing-available-com-ports-with-python
+    """
+    if sys.platform.startswith('win'):
+        ports = ['COM%s' % (i + 1) for i in range(2,256)]   #Except com1 (Communication port)
+    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+        # this excludes your current terminal "/dev/tty"
+        ports = glob.glob('/dev/tty[A-Za-z]*')
+    elif sys.platform.startswith('darwin'):
+        ports = glob.glob('/dev/tty.*')
+    else:
+        raise EnvironmentError('Unsupported platform')
+
+    result = []
+    for port in ports:
+        try:
+            s = serial.Serial(port,BAUD)
+            s.close()
+            result.append(port)
+        except (OSError, serial.SerialException):
+            pass
+    return result
+
+
+ser = serial_ports()
+print(ser[0], end=' ')
+s = serial.Serial(ser[0], BAUD)
+print('start')
+
+
+while True:
+    data = s.readline()
+    if 'DISCS' in str(data):
+        while 'DISCE' not in str(data):
+            data = s.readline()
+            if 'DISCS' not in str(data) and str(data).find(item) != -1:
+                count += 1
+            if 'DISCS' not in str(data) and str(data).find("Scatter") != -1: #demo
+                count += 1
+        print(f"{item} : {count}", end='    ')
+        datas = {    # item , count
+            'item' : item,
+            'count' : count
+        }
+        #send to server
+        try:
+            #response = requests.get(URL,params = datas)
+            response = requests.post(URL, data = datas)
+            print('===========    send')
+        except Exception as e:
+            print("===========    fail : ",e)
+        count = -1
+s.close()
+
+
